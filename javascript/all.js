00ba56ea90ce9605b8b34695af83941efb6a20a7
Bruno-Yu <jackhellowin@gmail.com>
2022-06-09 14:34:03 +0800

    add package js

--- javascript/all.js
+++ javascript/all.js
@@ -1,15 +1,5 @@
 import './plugins/jQuery.js';
 
-// index
-
-
-// pricing
-
-// slide toggle 
-// 將其都 做成expand 狀態
-// 使用slideToggle 將底層意見收起
-// 使用監聽 若slideToggle啟動 使用classToggle變換樣式
-
 
 
 // jQuery
@@ -18,6 +8,23 @@ $(function () {
 	$('.btn-toTop').on('click', function(){
 		$(window).scrollTop(0);
 	});
+	// calculate
+	// 思路: 先分離基本版data-num按鈕 data-ppl 人數 data-basic 與data-bis商頁板，再設立data-num做乘除使用，但data-num要設立最大值
+	// 後續再用.text()將相關數字帶入
+	$('.select-btn').on('click', function (e) { 
+		$(this).addClass('active').siblings().removeClass('active');
+		let ppl = $(this).attr('data-num');
+		if (ppl == 3) {
+				$('.ppl').text('>25000');
+		} else { 
+				$('.ppl').text(ppl*10000);
+		}
+	
+		$('.basic').text( ppl*600);
+		$(' .bis').text( ppl*1600 );
+
+	})
+
 	// Question toggle
 	$('.qna-item').on('click', function () { 
 		$(this).toggleClass('qna-item-show');
