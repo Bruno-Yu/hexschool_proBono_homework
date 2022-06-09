321ceb3c57868804006793afaba656c4d5214e60
Bruno-Yu <jackhellowin@gmail.com>
2022-06-09 11:06:09 +0800

    adjused

--- javascript/all.js
+++ javascript/all.js
@@ -1,12 +1,28 @@
-import "./plugins/jQuery.js";
+import './plugins/jQuery.js';
 
 // index
 
+
+// pricing
+
+// slide toggle 
+// 將其都 做成expand 狀態
+// 使用slideToggle 將底層意見收起
+// 使用監聽 若slideToggle啟動 使用classToggle變換樣式
+
+
+
 // jQuery
- $(function(){
-    $('.btn-toTop').on('click', function(){
-      $(window).scrollTop(0);
-    })
-  })
+$(function () {
+	// toTop
+	$('.btn-toTop').on('click', function(){
+		$(window).scrollTop(0);
+	});
+	// Question toggle
+	$('.qna-item').on('click', function () { 
+		$(this).toggleClass('qna-item-show');
+	})
+});
+  
 
   
